278be0f94e307629414d4aa34b57d7284fd5d0e4
sealtx72 <84211791+sealtx72@users.noreply.github.com>
2021-07-12 17:02:12 -0500

    update

--- JS/site.js
+++ JS/site.js
@@ -1,2 +1,49 @@
-/* CSS comments are different  Crt + k + c */
-// One comment line
+function getNumbers() {
+    /*let theDocument = document;
+    let theInput = theDocument.getElementById("startValue");
+    let value = theInput.value;*/
+    //access the webpage and get the values from the input
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //take the start and en values as the bounds for a 'for' loop
+    //that will create an array and return it
+
+    let numbers = generateNumbers(startValue, endValue);
+
+    //take the array that was returned and pass it to a function that will
+    //display it
+
+    displayNumbers(numbers);
+
+    //Wrapper function/method - a function that calls other functions
+}
+
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+
+    //loop over every number from startValue to endValue
+    for (let index = startValue; index <= endValue; index++) {
+        numbers.push(index)
+    }
+
+    return numbers;
+}
+
+function displayNumbers(numbers) {
+    let className = "even";
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+
+        if (number % 2 == 0) {
+            className = "even";
+        } else {
+            className = "odd";
+        }
+
+        templateRows = templateRows + `<tr><td class="${className}">${number}</td></tr>`;
+    }
+    document.getElementById("output").innerHTML = templateRows;
+}
