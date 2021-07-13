0063ce9e890b65653704bab933cc4743ec1eab07
sealtx72 <84211791+sealtx72@users.noreply.github.com>
2021-07-13 09:48:33 -0500

    Update

--- JS/site.js
+++ JS/site.js
@@ -3,25 +3,37 @@ function getNumbers() {
     let theInput = theDocument.getElementById("startValue");
     let value = theInput.value;*/
     //access the webpage and get the values from the input
+
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
+
+    //Convert startValue and endValue to integers
+
+    let newStart = parseInt(startValue);
+    let newEnd = parseInt(endValue);
+
+    // Validating newStart and newEnd
+
+    if (isNaN(newStart) || isNaN(newEnd)) {
+        alert("Please don't type the letter e!")
+        return;
+    }
 
     //take the start and en values as the bounds for a 'for' loop
     //that will create an array and return it
 
-    let numbers = generateNumbers(startValue, endValue);
+    let numbers = generateNumbers(newStart, newEnd);
 
     //take the array that was returned and pass it to a function that will
     //display it
 
     displayNumbers(numbers);
-
-    //Wrapper function/method - a function that calls other functions
 }
+//Wrapper function/method - a function that calls other functions
+
 
 function generateNumbers(startValue, endValue) {
     let numbers = [];
-
     //loop over every number from startValue to endValue
     for (let index = startValue; index <= endValue; index++) {
         numbers.push(index)
